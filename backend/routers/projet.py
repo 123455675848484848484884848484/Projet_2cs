1924a8d4af01e56c07053a3ea4cba79a6f2f97b5
sraaasaaa <ls_aitsiamer@esi.dz>
2025-05-08 12:17:49 +0100

    informations sur un puit (api)

--- backend/routers/projet.py
+++ backend/routers/projet.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import get_db
-from models import Projet, UserProjet
+from models import Projet, UserProjet, Utilisateur
 from pys_models import ProjetCreate, ProjetOut,AffectationCreate
 
 router = APIRouter(prefix="/projets", tags=["Projets"])
@@ -49,3 +49,59 @@ def affecter_utilisateur_a_projet(affectation: AffectationCreate, db: Session = 
         "id_utilisateur": affectation.id_utilisateur,
         "id_projet": affectation.id_projet
     }
+
+
+@router.get("/{id_utilisateur}")
+def get_projets_par_utilisateur(id_utilisateur: int, db: Session = Depends(get_db)):
+    projets_ids = db.query(UserProjet.id_projet).filter(UserProjet.id_utilisateur == id_utilisateur).all()
+
+    if not projets_ids:
+        raise HTTPException(status_code=404, detail="Aucun projet trouvé pour cet utilisateur.")
+
+
+    projets_ids = [pid for (pid,) in projets_ids]
+    projets = db.query(Projet).filter(Projet.id.in_(projets_ids)).all()
+
+    return [  
+        {
+            "name": projet.name,
+            "date_debut": projet.date_debut,
+            "budget_total": projet.budget_total,
+            "duree_prevue": projet.duree_prevue,
+            "wilaya": projet.wilaya,
+            "adresse": projet.adresse,
+        }
+        for projet in projets
+    ]
+    
+
+@router.get("/{id_projet}/details")
+def get_projet_avec_equipe(id_projet: int, db: Session = Depends(get_db)):
+    # Récupérer le projet
+    projet = db.query(Projet).filter(Projet.id == id_projet).first()
+    if not projet:
+        raise HTTPException(status_code=404, detail="Projet non trouvé.")
+
+    # Récupérer les IDs des utilisateurs liés au projet
+    utilisateur_ids = db.query(UserProjet.id_utilisateur).filter(UserProjet.id_projet == id_projet).all()
+    utilisateur_ids = [uid for (uid,) in utilisateur_ids]
+
+    # Récupérer uniquement les noms
+    noms = db.query(Utilisateur.name).filter(Utilisateur.id.in_(utilisateur_ids)).all()
+    noms_equipes = [nom for (nom,) in noms]
+
+    # Réponse JSON
+    return {
+        "id": projet.id,
+        "name": projet.name,
+        "date_debut": str(projet.date_debut),
+        "created_by": projet.created_by,
+        "lieu": projet.lieu,
+        "budget_total": float(projet.budget_total or 0),
+        "duree_prevue": projet.duree_prevue,
+        "wilaya": projet.wilaya,
+        "adresse": projet.adresse,
+        "closed": projet.closed,
+        "equipe": noms_equipes
+    }
+
