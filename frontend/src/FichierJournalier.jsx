55cace46ebfa289319630ec4a02012e5d37bd241
Maya Rial <mayarial@MacBook-Air-de-Maya.local>
2025-06-07 22:54:50 +0100

    puit dans fichier

--- frontend/src/FichierJournalier.jsx
+++ frontend/src/FichierJournalier.jsx
@@ -64,13 +64,28 @@ const FichierJournalier = () => {
 
   return (
     <div className="min-h-screen px-24 py-12 bg-[#f4f4f4]">
-      <h1 className="text-[54px] font-bold text-orange-600 leading-[60px] mb-2">
+      
+      <h1 className="text-[54px] font-bold text-[#EA5529] leading-[60px] mb-2">
         Fichier journalier
       </h1>
       <p className="text-gray-700 mb-8">
         For marketplace sellers looking to grow their business, metaverse offers the best platform.
       </p>
-
+      <div className="mb-8">
+  <label className="block mb-2 text-gray-700 font-semibold text-lg">
+    Sélectionner le puits
+  </label>
+  <select
+  className="w-[300px] h-12 border border-gray-300 rounded-md px-3 text-base bg-white text-gray-800 focus:outline-none focus:border-orange-600 focus:ring-1 focus:ring-orange-600 transition"
+  defaultValue=""
+  onChange={(e) => console.log("Puit sélectionné :", e.target.value)}
+>
+  <option value="" disabled>-- Choisir un puits --</option>
+  <option value="Puit 1">Puit 1</option>
+  <option value="Puit 2">Puit 2</option>
+  <option value="Puit 3">Puit 3</option>
+</select>
+</div>
       <label className="bg-[#2f5744] cursor-pointer text-white text-center text-[20px] font-semibold py-5 rounded-md flex justify-center items-center gap-4 mb-4">
   <img src="/file.png" alt="icon" className="w-6 h-6" />
   Importer le fichier journalier
@@ -126,7 +141,7 @@ const FichierJournalier = () => {
                     <div className="grid grid-cols-4 gap-6 mt-4">
                       {operationsGroups.map((group) => (
                         <div key={group.label}>
-                          <h4 className="text-xs text-orange-600 font-semibold mb-2">
+                          <h4 className="text-xs text-[#EA5529] font-semibold mb-2">
                             {group.label}
                           </h4>
                           {group.items.map((item) => (
@@ -152,14 +167,14 @@ const FichierJournalier = () => {
                     type="text"
                     value={row.probleme}
                     onChange={(e) => handleChange(index, "probleme", e.target.value)}
-                    className="w-full border border-gray-300 rounded px-2 py-1 focus:outline-none focus:ring-0 focus:border-orange-600"                  />
+                    className="w-full border border-gray-300 rounded px-2 py-1 focus:outline-none focus:ring-0 focus:border-[#EA5529]"                  />
                 </td>
                 <td className="px-4 py-2 align-top">
                   <input
                     type="text"
                     value={row.solution}
                     onChange={(e) => handleChange(index, "solution", e.target.value)}
-                    className="w-full border border-gray-300 rounded px-2 py-1 focus:outline-none focus:ring-0 focus:border-orange-600"                  />
+                    className="w-full border border-gray-300 rounded px-2 py-1 focus:outline-none focus:ring-0 focus:border-[#EA5529]"                  />
                 </td>
                 <td className="px-4 py-2 align-top">
   {!row.file ? (
@@ -190,7 +205,7 @@ const FichierJournalier = () => {
 
       <h2 className="text-xl font-semibold mb-2">Autres problèmes</h2>
       <textarea
-  className="w-full h-24 border border-gray-300 rounded px-2 py-1 focus:outline-none focus:ring-0 focus:border-orange-600"
+  className="w-full h-24 border border-gray-300 rounded px-2 py-1 focus:outline-none focus:ring-0 focus:border-[#EA5529]"
   placeholder=""
         value={autresProblemes}
         onChange={(e) => setAutresProblemes(e.target.value)}
@@ -199,7 +214,7 @@ const FichierJournalier = () => {
       <div className="flex justify-end">
         <button
           onClick={handleSubmit}
-          className="bg-orange-600 hover:bg-orange-700 text-white font-semibold text-[16px] px-10 py-3 rounded-md"
+          className="bg-[#EA5529] hover:bg-orange-700 text-white font-semibold text-[16px] px-10 py-3 rounded-md"
         >
           Valider
         </button>
