c325d648bef8a2a889e6c44a412ff8e3ec5825c9
nxnrrr <ln_fadel@esi.dz>
2025-06-09 19:58:25 +0100

    nothing much

--- backend/routers/utilisateur.py
+++ backend/routers/utilisateur.py
@@ -12,6 +12,10 @@ router = APIRouter(prefix="/utilisateur",
 def get_utilisateurs(db: Session = Depends(get_db)):
     return db.query(Utilisateur).all()
 
+@router.get("/{user_id}", response_model=list[UtilisateurOut])
+def get_utilisateurs(user_id: int,db: Session = Depends(get_db)):
+    return db.query(Utilisateur).filter(Utilisateur.id == user_id).first()
+
 @router.post("/", response_model=UtilisateurOut)
 def create_utilisateur(user: UtilisateurCreate, db: Session = Depends(get_db)):
     db_user = Utilisateur(**user.dict())
