7ba008900b25abd895354fc3ed89ebed53d2c7fe
sraaasaaa <ls_aitsiamer@esi.dz>
2025-06-11 18:06:31 +0100

    navbar some modif

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({ role }) => {
   const navigate = useNavigate();
   return (
     <nav className="sticky top-0 z-50 w-full bg-white shadow-md px-6 py-4 flex items-center justify-between">
-      
+
       {/* Left - Logo */}
       <div className="flex items-center">
         <img src={logo} alt="Logo" className="h-12 w-auto" />
@@ -15,21 +15,37 @@ const Navbar = ({ role }) => {
       {/* Right - Navigation + Button */}
       <div className="flex items-center space-x-6">
         <ul className="hidden md:flex space-x-6 text-gray-700 font-medium items-center">
-          <li className="hover:text-[#EA5529] cursor-pointer">Accueil</li>
+          <li
+            className="hover:text-[#EA5529] cursor-pointer"
+            onClick={() => {
+              if (role === "manager") {
+                navigate("/manager");
+              }
+            }}
+          >
+            Accueil
+          </li>
+
 
           {role === "manager" && (
-            <li className="hover:text-[#EA5529] cursor-pointer">Mon compte</li>
+            <li
+              className="hover:text-[#EA5529] cursor-pointer"
+              onClick={() => navigate("/moncompte")}
+            >
+              Mon compte
+            </li>
           )}
+
 
           {role === "admin" && (
             <>
-              <li 
+              <li
                 className="hover:text-[#EA5529] cursor-pointer"
-                onClick={() => navigate("/creeruser")}
+                onClick={() => navigate("/mescomptes")}
               >
                 Consulter les comptes
               </li>
-              <li 
+              <li
                 className="hover:text-[#EA5529] cursor-pointer"
                 onClick={() => navigate("/alerts")}
               >
@@ -41,10 +57,14 @@ const Navbar = ({ role }) => {
 
         {/* Role-based Button */}
         {role === "manager" && (
-          <button className="bg-[#EA5529] text-white px-4 py-2 rounded-lg hover:bg-[#d1441f] transition">
+          <button
+            onClick={() => navigate("/mespuits")}
+            className="bg-[#EA5529] text-white px-4 py-2 rounded-lg hover:bg-[#d1441f] transition"
+          >
             Consulter Mes puits
           </button>
         )}
+
         {role === "guest" && (
           <button className="bg-[#EA5529] text-white px-4 py-2 rounded-lg hover:bg-[#d1441f] transition">
             Se connecter
